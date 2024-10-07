4710c2355f3bee192099d1c679c403c633ef0625
roypunga <royseba2005@gmail.com>
2024-10-07 10:16:03 -0300

    Crear estructuras y main vacio

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <time.h>
+
+struct struct_usuario {
+    long int cuil;
+    char nombre[50];
+    long int celular;
+    char email[30];
+    long int cvu;
+    char alias[30];
+    int iva;
+    float saldo;
+};
+
+struct struct_cuenta {
+    int tipo; //caja de ahorro cuenta corriente
+    long int cbu;
+    long int cuil;
+};
+
+struct struct_movimiento{
+    int tipo; //ingreso transferencia o pago
+    float monto;
+    float iibb;
+    //fecha 
+    long int cuilRecibe;
+    long int cuilEnvia;
+}
+int main() {
+    return 0;
+}
